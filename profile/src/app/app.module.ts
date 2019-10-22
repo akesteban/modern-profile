78bb54d1228baf48e5ad1c9b4bc2afba6dd7c2f9
laura.micek <laura.micek@fmr.com>
2019-10-22 11:17:59 -0400

    Created angular project with cli and basic setup

---
+++ profile/src/app/app.module.ts
@@ -0,0 +1,22 @@
+import { BrowserModule } from '@angular/platform-browser';
+import { NgModule } from '@angular/core';
+
+import { AppRoutingModule } from './app-routing.module';
+import { AppComponent } from './app.component';
+import { HeaderComponent } from './layout/header/header/header.component';
+import { NavigationComponent } from './layout/header/navigation/navigation.component';
+
+@NgModule({
+  declarations: [
+    AppComponent,
+    HeaderComponent,
+    NavigationComponent
+  ],
+  imports: [
+    BrowserModule,
+    AppRoutingModule
+  ],
+  providers: [],
+  bootstrap: [AppComponent]
+})
+export class AppModule { }
